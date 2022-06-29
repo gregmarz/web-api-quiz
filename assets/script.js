3e23a093063a131161bbd43ec06912537c0fc098
marzec <gregmarzec02@gmail.com>
2022-06-29 01:28:32 -0400

    finished game over page

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,17 @@ var timer = document.getElementById("timer");
 var startBtn = document.getElementById("btnStart");
 var startPage = document.getElementById("startPage");
 var qTimer = document.getElementById("qTimer");
+var youLost = document.getElementById("LOSELOSE");
+var endPage = document.getElementById("endPage");
 var questPage = document.getElementById("questContainer");
 var questionOne = document.getElementById("questionOne");
 var questionTwo = document.getElementById("questionTwo");
 var subBtn = document.getElementById("btnSubmit");
 var subBtn2 = document.getElementById("btnSubmit2");
 var qRight = document.getElementById("rightQ");
-var HELPHELP = document.getElementById("rightQ2");
+var qRight2 = document.getElementById("rightQ2");
 
-
-var timeLeft = 100;
+var timeLeft = 60;
 var numWrong = 0;
 var numRight = 0;
 
@@ -20,14 +21,18 @@ function timerInt() {
         if (timeLeft >= 0) {
             timer.innerHTML = "Time Left: " + timeLeft;
             timeLeft--;
-            return
-        } else {
-            if (timeLeft <=1) {
-                alert("You Lose!")
-                return
+        } 
+        else {
+            if (timeLeft <= 1) {
+                questionOne.setAttribute("class","hidden");
+                questionTwo.setAttribute("class","hidden");
+                timer.setAttribute("class","hidden");
+                endPage.setAttribute("class","");
+                questPage.setAttribute("class","hidden");
+                qTimer.setAttribute("class","hidden");
             }
         }
-    },1000)
+    }, 1000)
 };
 
 function checkAns1() {
@@ -45,11 +50,15 @@ function checkAns1() {
 };
 
 function checkAns2() {
-    if ((HELPHELP).checked) {
+    if ((qRight2).checked) {
         numRight++;
         alert("good job");
-        console.log("amount right: " + numRight);
-        questionTwo.setAttribute("class","");
+        questionOne.setAttribute("class","hidden");
+        questionTwo.setAttribute("class","hidden");
+        timer.setAttribute("class","hidden");
+        endPage.setAttribute("class","");
+        questPage.setAttribute("class","hidden");
+        qTimer.setAttribute("class","hidden");
     } else {
         numWrong++;
         alert("wrong!")
